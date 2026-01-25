9a9809c28dcc7fb437792be89c9802ebbabb1a00
tBeltty <jhocomo@gmail.com>
2026-01-24 21:56:53 -0500

    fix(build): update vite alias to point to src_new/src

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename)
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src_new/src'),
     },
   },
   plugins: [
